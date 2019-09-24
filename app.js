b56f94cdf23c6370c509b615d306ea5125aaf800
Audelio Lujan <alujan@foundationmedicine.com>
2019-09-23 18:44:44 -0700

    Graphql First seen

--- app.js
+++ app.js
@@ -1,6 +1,14 @@
 const express = require('express')
+const grapqlHTTP = require('express-graphql')
+
+const schema = require('./schema/schema')
 
 const app = express()
+
+app.use('/graphql', grapqlHTTP({
+  schema,
+  graphiql:true
+}))
 
 app.listen(2002, () => {
   console.log('Server start on port 2002')
---
+++ schema/schema.js
@@ -0,0 +1,74 @@
+const gql = require('graphql')
+const _ = require('lodash')
+
+const { 
+  GraphQLObjectType,
+  GraphQLString,
+  GraphQLID,
+  GraphQLInt,
+  GraphQLSchema
+} = gql
+
+const books = [
+  { name: 'Name if 1', genre: 'Fantasy', id: '1', authorId: '1' },
+  { name: 'Name if 2', genre: 'Fantasy', id: '2', authorId: '2' },
+  { name: 'Name if 3', genre: 'Si-Fi', id: '3', authorId: '3' }
+]
+
+const authors = [
+  { name: 'Name author 1', age: '28', id: '1' },
+  { name: 'Name author 2', age: '30', id: '2' },
+  { name: 'Name author 3', age: '45', id: '3' }
+]
+
+const BookType = new GraphQLObjectType({
+  name: 'Book',
+  fields: () => ({
+    id: { type: GraphQLID },
+    name: { type: GraphQLString },
+    genre: { type: GraphQLString },
+    author: { 
+      type: AuthorType,
+      resolve(parent, args) {
+        return _.find(authors, { id: parent.authorId })
+      }
+    }
+  })
+})
+
+const AuthorType = new GraphQLObjectType({
+  name: 'Author',
+  fields: () => ({
+    id: { type: GraphQLID },
+    name: { type: GraphQLString },
+    age: { type: GraphQLInt }
+  })
+})
+
+const RootQuery = new GraphQLObjectType({
+  name: 'RootQuerytype',
+  fields: {
+    book: {
+      type: BookType,
+      args: { 
+        id: { type: GraphQLID }
+      },
+      resolve(parent, args) {
+        return _.find(books, { id: args.id })
+      }
+    },
+    author: {
+      type: AuthorType,   
+      args: { 
+        id: { type: GraphQLID }
+      },
+      resolve(parent, args) {
+        return _.find(authors, { id: args.id })
+      }
+    }
+  }
+})
+
+module.exports = new GraphQLSchema({
+  query: RootQuery
+})
