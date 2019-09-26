117993f2818679e996984d3f809eba3d2b3e0ac4
Audelio Lujan <alujan@foundationmedicine.com>
2019-09-25 19:20:20 -0700

    cors

--- app.js
+++ app.js
@@ -2,11 +2,14 @@ const express = require('express')
 const grapqlHTTP = require('express-graphql')
 const schema = require('./schema/schema')
 const mongoose = require('mongoose')
+const cors = require('cors')
 
 const app = express()
 
-mongoose
-  .connect('')
+app.use(cors())
+
+mongoose 
+  .connect()
   .catch(err => {
     console.log(err)
   })
